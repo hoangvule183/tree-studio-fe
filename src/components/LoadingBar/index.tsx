337ce4107bac687b630bb743f2a7798c69b1468e
vulh <vulh@uniultra.xyz>
2024-09-12 17:21:55 +0700

    feat: add loading perentage text

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -1,16 +1,34 @@
 export default function LoadingBar({ percentage = 0 }: { percentage: number }) {
   return (
     <div className="w-full flex items-end relative justify-center">
-      <img src="/images/common/loading-bar.png" className="w-[40%] mx-auto bg-[#606060]" />
-      <div className="absolute w-[39%] aspect-[14] mx-auto mb-[0.3%] flex bg-[#606060] gap-[10px] px-[10px] py-[5px] flex-wrap items-center justify-start">
-        {Array(Math.floor(percentage / 5)).fill("").map((_, i) => 
-        <div key={i} 
-        style={{
-            width: 'calc((100% / 20) - 9.5px)',
-        }}
-        className="h-full bg-[white]"/>
-        )}
-        </div>
+      <img
+        src="/images/common/loading-bar.png"
+        className="w-[40%] mx-auto bg-[#606060]    max-[725px]:w-[100%]"
+      />
+      <h1
+        className="font-['UpheavalPro'] text-[white] absolute w-[35%] 
+          max-[1500px]:text-[2rem]
+          max-[1000px]:text-[1.5rem]         
+          max-[725px]:text-[2rem] max-[725px]:w-[10%]
+        right-[0] -top-[5px] z-[100] text-[3rem]"
+      >
+        {Math.floor(percentage)}%
+      </h1>
+      <div className="absolute w-[39%] 
+       max-[725px]:w-[99%]
+      aspect-[14] mx-auto mb-[0.3%] flex bg-[#606060] gap-[10px] px-[10px] py-[5px] flex-wrap items-center justify-start">
+        {Array(Math.floor(percentage / 5))
+          .fill("")
+          .map((_, i) => (
+            <div
+              key={i}
+              style={{
+                width: "calc((100% / 20) - 9.5px)",
+              }}
+              className="h-full bg-[white]"
+            />
+          ))}
+      </div>
     </div>
   );
 }
